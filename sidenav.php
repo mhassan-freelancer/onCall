c12e043fa14ebf9f9eb2ddc6b4e0e61f4b811052
Muhammad Hassan <mhassan.freelancer@gmail.com>
2016-08-11 10:57:18 +0500

    Home Menu
    For all Users

--- sidenav.php
+++ sidenav.php
@@ -1,14 +1,17 @@
 <?php
+require 'includes/config.php';
+?>
+<li><a href="<?php echo BASE_URL ?>"><i class="fa fa-circle-o text-red"></i> <span>Home</span></a></li>
+<?php
 if($_SESSION['on_call_is_super_admin'] == 1)
 {
     ?>
-    <li><a href=""><i class="fa fa-circle-o text-red"></i> <span>Home</span></a></li>
     <li><a href="system_users.php"><i class="fa fa-circle-o text-yellow"></i> <span>User Manager</span></a></li>
     <li><a href="system_details.php"><i class="fa fa-circle-o text-red"></i> <span>Alarm</span></a></li>
     <li><a href="parameters.php"><i class="fa fa-circle-o text-yellow"></i> <span>Parameter</span></a></li>
     <li><a href="config.php"><i class="fa fa-circle-o text-yellow"></i> <span>Config</span></a></li>
     <li><a href="events.php"><i class="fa fa-circle-o text-yellow"></i> <span>Events</span></a></li>
-    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>
+<!--    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>-->
     <li><a href="update_page.php"><i class="fa fa-circle-o text-yellow"></i> <span>Settings</span></a></li>
     <?php
 }
@@ -17,7 +20,7 @@ else if ($_SESSION['on_call_is_admin'] == 1)
     ?>
     <li><a href="system_users.php"><i class="fa fa-circle-o text-yellow"></i> <span>User Manager</span></a></li>
     <li><a href="system_details.php"><i class="fa fa-circle-o text-red"></i> <span>Alarm</span></a></li>
-    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>
+<!--    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>-->
 
     <?php
 }
@@ -25,12 +28,7 @@ else if ($_SESSION['on_call_is_oncall'] == 1)
 {
     ?>
     <li><a href="system_details.php"><i class="fa fa-circle-o text-red"></i> <span>Alarm</span></a></li>
-    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>
-    <?php
-}
-else
-{
-    ?>
-    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>
+<!--    <li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>-->
     <?php
 }?>
+<li><a href="account_settings.php"><i class="fa fa-circle-o text-yellow"></i> <span>My Account</span></a></li>
